1ddedf1005c999bf0133f244d1318d5ff862735c
Mikkel E Lepperød <lep.mik@gmail.com>
2017-05-13 21:59:50 +0200

    add the shabang

---
+++ expipe_plugin_cinpla/__init__.py
@@ -0,0 +1,6 @@
+from .main import CinplaPlugin
+from .axona import AxonaPlugin
+from .openephys import OpenEphysPlugin
+from .intan import IntanPlugin
+from .optogenetics import OptoPlugin
+from .make_spatiality_overview import make_spatiality_overview
