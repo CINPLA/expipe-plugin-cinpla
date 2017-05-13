3877977707c1caa60d111eacbf247012b25003ab
Mikkel E Lepperød <lep.mik@gmail.com>
2017-05-13 22:02:05 +0200

    version handling

--- expipe_plugin_cinpla/__init__.py
+++ expipe_plugin_cinpla/__init__.py
@@ -4,3 +4,6 @@ from .openephys import OpenEphysPlugin
 from .intan import IntanPlugin
 from .optogenetics import OptoPlugin
 from .make_spatiality_overview import make_spatiality_overview
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
