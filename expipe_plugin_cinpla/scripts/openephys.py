233412c28c51d78d72e9bd3ac7a082b32f7e95d4
Can Hicabi Tartanoglu <canhtart@gmail.com>
2019-02-02 07:24:48 +0100

    epc->scripts->openephys new function to read psychopy data into exdir

--- expipe_plugin_cinpla/scripts/openephys.py
+++ expipe_plugin_cinpla/scripts/openephys.py
@@ -410,3 +410,49 @@ def process_openephys(project, action_id, probe_path, sorter, acquisition_folder
     print("Total elapsed time: ", time.time() - proc_start)
 
 
+def _process_psychopy(jsonpath, exdir_path="."):
+        '''
+        keys = ["image", "sparsenoise", "grating", "sparsenoise", "movie"]
+        valuekeys = ["duration", "image", "phase", "spatial_frequency", "frequency", "orientation", "movie"]
+        {"image": {"duration": 0.25, "image": "..\\datasets\\converted_images\\image0004.png"}}
+        {"sparsenoise": {"duration": 0.25, "image": "..\\datasets\\sparse_noise_images\\image0022.png"}}
+        {"grating": {"duration": 0.25, "phase": 0.5, "spatial_frequency": 0.16, "frequency": 0, "orientation": 120}}
+        {"movie": {"movie": "..\\datasets\\converted_movies\\segment1.mp4"}}
+        {"grating": {"phase": "f*t", "duration": 2.0, "spatial_frequency": 0.04, "frequency": 4, "orientation": 225}}
+        {"grayscreen" : {"duration": 300.}}
+        '''
+        import os
+        import json
+
+
+        print("Converting PsychoPy data to exdir format")
+                
+        json_data = []
+        with open(stimfiles, 'r') as f:
+            for line in f.readlines():
+                json_line = line.replace("'", '"')
+                try:
+                    json_data.append(json.loads(json_line))
+                except json.JSONDecodeError:
+                    print("Skipping line. The line is not in json format:\n{}".format(line))
+
+        exdir_path = os.path.join(exdir_path, "main.exdir")
+        exdir_file = exdir.File(exdir_path)
+        epochs = exdir_file.require_group("epochs")
+        psychopy = epochs.require_group("psychopy")
+
+        keys = json_data.keys()
+        data_type_setsize = len(set(keys))
+        if data_type_setsize == 1:
+            key = keys()[0]
+            psychopy.require_dataset('timestamp', data=time[key])
+            psychopy.require_dataset('duration', data=duration[key])
+            psychopy.require_dataset('orrientation', data=orrientation[key])
+
+        elif data_type_setsize > 1:
+            for key in json_data.keys():
+                psychopy.require_dataset('timestamp', data=time[key])
+                psychopy.require_dataset('duration', data=duration[key])
+                psychopy.require_dataset('orrientation', data=orrientation[key])
+
+        print("Done. The data is located in {}".format(exdir_path))
